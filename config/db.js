2c91ea0de09105a065d2062448822525e1c0c559
N.Raju <151713861+NalagamdinniRaju@users.noreply.github.com>
2024-11-23 17:08:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/notes-manager', {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
